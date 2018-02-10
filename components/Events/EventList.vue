3ff2af887c67d4e73f195f9bbcf02e7ae2275d7b
Diky Arga <dikyarga.id@gmail.com>
2018-02-11 01:18:56 +0700

    create EventList component

---
+++ components/Events/EventList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <event-preview slug="moz-belajar-css-grid" title="#MozBelajar CSS Grid" organization="Mozilla Indonesia" description="Anggap aja ini description dulu ya" />
+  </div>  
+</template>
+
+<script>
+import EventPreview from '@/components/Events/EventPreview';
+
+export default {
+  name: 'EventList',
+  components: {
+    EventPreview,
+  },
+}
+</script>
