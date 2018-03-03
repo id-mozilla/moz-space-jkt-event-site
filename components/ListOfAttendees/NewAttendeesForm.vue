d366e4fc2caf50292795b5d4c1e4fc3cba0ada5b
Diky Arga <dikyarga.id@gmail.com>
2018-03-03 15:29:11 +0700

    decouple NewAttendessForm

---
+++ components/ListOfAttendees/NewAttendeesForm.vue
@@ -0,0 +1,134 @@
+<template>
+  <form class="full pa-4">
+    <v-text-field
+      v-model="name"
+      label="Nama kamu"
+      :error-messages="errors.collect('name')"
+      v-validate="'required'"
+      data-vv-name="name"
+      required
+    ></v-text-field>
+    <v-select
+      v-bind:items="occupationOptions"
+      v-model="occupation"
+      label="Kesibukan sehari-hari"
+      :error-messages="errors.collect('occupation')"
+      v-validate="'required'"
+      data-vv-name="occupation"
+      required
+    ></v-select>
+    <v-text-field
+      v-model="email"
+      label="Email kamu"
+      :error-messages="errors.collect('email')"
+      v-validate="'required|email'"
+      data-vv-name="email"
+      required
+    ></v-text-field>
+    <v-text-field
+      v-model="phone"
+      label="Nomor ponsel / WA kamu"
+      :error-messages="errors.collect('phone')"
+      v-validate="'required'"
+      data-vv-name="phone"
+      required
+    ></v-text-field>
+    <v-select
+      v-bind:items="genderOptions"
+      v-model="gender"
+      label="Jenis kelamin"
+      :error-messages="errors.collect('gender')"
+      v-validate="'required'"
+      data-vv-name="gender"
+      required
+    ></v-select>
+    <h4>Apakah kamu developer / programmer ?</h4>
+    <v-radio-group v-model="isDeveloper" :mandatory="false">
+      <v-radio label="Ya" value="true"></v-radio>
+      <v-radio label="Tidak" value="false"></v-radio>
+    </v-radio-group>
+    <v-btn large block @click="submit" color="primary" class="mt-3">Saya Hadir</v-btn>
+  </form>
+</template>
+<script>
+export default {
+  $_veeValidate: {
+    validator: 'new'
+  },
+    data() {
+    return {
+      name: '',
+      occupation: '',
+      occupationOptions: [
+        'Berkerja',
+        'Kuliah',
+        'Pelajar',
+        'Lainnya'
+      ],
+      gender: '',
+      genderOptions: [
+        'Laki-laki',
+        'Perempuan'
+      ],
+      email: '',
+      phone: '',
+      isDeveloper: true,
+      yearOfBirth: 0,
+      dictionary: {
+        custom: {
+          name: {
+            required: 'Nama harus diisi ya, ingat tak kenal maka tak sayang :D'
+          },
+          occupation: {
+            required: 'Sekarang lagi sibuk apa ya sehari-hari ?'
+          },
+          gender: {
+            required: 'Mbak-mbak atau Mas-mas ya ?'
+          },
+          email: {
+            required: 'Kami butuh email kamu supaya kalo ada acara bagus kami bisa kasih tahu',
+            email: 'Emailnya belum valid, Kak'
+          },
+          phone: {
+            required: 'Kami bisa kasih tahu kalo ada kegiatan menarik'
+          }
+        }
+      }
+    }
+  },
+  methods: {
+    submit() {
+      this.$validator.validateAll().then(isFormValid => {
+        if (isFormValid) {
+         this.$axios.$post(`/Events/${this.$route.params.id}/participants`, {
+           name: this.name,
+           occupation: this.occupation,
+           gender: this.gender,
+           email: this.email,
+           phone: this.phone,
+           yearOfBirth: this.yearOfBirth,
+           isDeveloper: this.isDeveloper,
+         }).then(result => {
+           console.log('result : ', result)
+           this.successDialog = true
+           this.clearForm()
+         }).catch(err => {
+           console.log('error when trying to post participant', err)
+         })
+        }
+      })
+    },
+    clearForm(){
+      this.name = ''
+      this.email = ''
+      this.phone = ''
+      this.gender = ''
+      this.occupation = ''
+      this.$nextTick(() => this.$validator.reset())
+    },
+  },
+  mounted() {
+    this.$validator.localize('en', this.dictionary)
+  }
+}
+</script>
--- pages/admin/events/_id/list-of-attendees.vue
+++ pages/admin/events/_id/list-of-attendees.vue
@@ -5,56 +5,21 @@
       <h3 class="headline text-xs-center">Absensi daftar hadir peserta</h3>
       <h4 class="title text-xs-center mt-3 orange--text" v-if="event">{{ event.title }}</h4>
 
-      <form class="full pa-4">
-        <v-text-field
-          v-model="name"
-          label="Nama kamu"
-          :error-messages="errors.collect('name')"
-          v-validate="'required'"
-          data-vv-name="name"
-          required
-        ></v-text-field>
-        <v-select
-          v-bind:items="occupationOptions"
-          v-model="occupation"
-          label="Kesibukan sehari-hari"
-          :error-messages="errors.collect('occupation')"
-          v-validate="'required'"
-          data-vv-name="occupation"
-          required
-        ></v-select>
-        <v-text-field
-          v-model="email"
-          label="Email kamu"
-          :error-messages="errors.collect('email')"
-          v-validate="'required|email'"
-          data-vv-name="email"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="phone"
-          label="Nomor ponsel / WA kamu"
-          :error-messages="errors.collect('phone')"
-          v-validate="'required'"
-          data-vv-name="phone"
-          required
-        ></v-text-field>
-        <v-select
-          v-bind:items="genderOptions"
-          v-model="gender"
-          label="Jenis kelamin"
-          :error-messages="errors.collect('gender')"
-          v-validate="'required'"
-          data-vv-name="gender"
-          required
-        ></v-select>
-        <h4>Apakah kamu developer / programmer ?</h4>
-        <v-radio-group v-model="isDeveloper" :mandatory="false">
-          <v-radio label="Ya" value="true"></v-radio>
-          <v-radio label="Tidak" value="false"></v-radio>
-        </v-radio-group>
-        <v-btn block @click="submit" color="primary" class="mt-3">Saya Hadir</v-btn>
-      </form>
+      <v-stepper v-model="attendeeStep" vertical class="mt-4">
+        <v-stepper-step step="1" complete>
+          Sudah pernah kesini ?
+        </v-stepper-step>
+        <v-stepper-content step="1">
+          
+          <v-btn color="primary" @click.native="attendeeStep = 2">Saya pernah kesini</v-btn>
+        </v-stepper-content>
+        <v-stepper-step step="2" complete>Isi data berikut ya</v-stepper-step>
+        <v-stepper-content step="2">
+          <new-attendees-form></new-attendees-form>
+        </v-stepper-content>
+      </v-stepper>
+
+      
       <v-dialog v-model="successDialog" persistent max-width="290">
         <v-card>
           <v-card-title class="headline">Sudah masuk absensi</v-card-title>
@@ -78,84 +43,18 @@
 
 <script>
 import Logo from '~/components/Logo.vue';
+import NewAttendeesForm from '~/components/ListOfAttendees/NewAttendeesForm';
 
 export default {
-  $_veeValidate: {
-    validator: 'new'
-  },
   data() {
     return {
       event: {},
       successDialog: false,
-      name: '',
-      occupation: '',
-      occupationOptions: [
-        'Berkerja',
-        'Kuliah',
-        'Pelajar',
-        'Lainnya'
-      ],
-      gender: '',
-      genderOptions: [
-        'Laki-laki',
-        'Perempuan'
-      ],
-      email: '',
-      phone: '',
-      isDeveloper: true,
-      yearOfBirth: 0,
-      dictionary: {
-        custom: {
-          name: {
-            required: 'Nama harus diisi ya, ingat tak kenal maka tak sayang :D'
-          },
-          occupation: {
-            required: 'Sekarang lagi sibuk apa ya sehari-hari ?'
-          },
-          gender: {
-            required: 'Mbak-mbak atau Mas-mas ya ?'
-          },
-          email: {
-            required: 'Kami butuh email kamu supaya kalo ada acara bagus kami bisa kasih tahu',
-            email: 'Emailnya belum valid, Kak'
-          },
-          phone: {
-            required: 'Kami bisa kasih tahu kalo ada kegiatan menarik'
-          }
-        }
-      }
+      attendeeStep: 1,
+      firstVisit: true,
     }
   },
   methods: {
-    submit() {
-      this.$validator.validateAll().then(isFormValid => {
-        if (isFormValid) {
-         this.$axios.$post(`/Events/${this.$route.params.id}/participants`, {
-           name: this.name,
-           occupation: this.occupation,
-           gender: this.gender,
-           email: this.email,
-           phone: this.phone,
-           yearOfBirth: this.yearOfBirth,
-           isDeveloper: this.isDeveloper,
-         }).then(result => {
-           console.log('result : ', result)
-           this.successDialog = true
-           this.clearForm()
-         }).catch(err => {
-           console.log('error when trying to post participant', err)
-         })
-        }
-      })
-    },
-    clearForm(){
-      this.name = ''
-      this.email = ''
-      this.phone = ''
-      this.gender = ''
-      this.occupation = ''
-      this.$nextTick(() => this.$validator.reset())
-    },
     fetchEventInformation(id) {
       this.$axios.$get(`/Events/${id}`).then(event => {
         this.event = event
@@ -167,11 +66,9 @@ export default {
   created() {
     this.fetchEventInformation(this.$route.params.id)
   },
-  mounted() {
-    this.$validator.localize('en', this.dictionary)
-  },
   components: {
     Logo,
+    NewAttendeesForm,
   }
 }
 </script>
