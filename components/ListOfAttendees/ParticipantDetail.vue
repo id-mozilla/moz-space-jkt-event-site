55015c38a8c9c41f840e792d31002608475f4fb9
Diky Arga <dikyarga.id@gmail.com>
2018-03-04 15:16:52 +0700

    create Participant Detail after select from existing attendees form

---
+++ components/ListOfAttendees/ParticipantDetail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2 class="body-2 text-xs-center pb-2">Apakah informasi dibawah ini benar ?</h2>
+    <h4 class="caption">email</h4>
+    <h3 class="subheading">{{ participant.email }}</h3>
+    <h4 class="caption">nomor telephone</h4>
+    <h3 class="subheading">{{ participant.phone }}</h3>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'ParticipantDetail',
+  props: ['participant'],
+}
+</script>
