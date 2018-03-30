41459d9ab77f7284b12e11abcba884fbce1ec725
dikyarga <dikyarga.id@gmail.com>
2018-03-30 23:09:06 +0700

    setup socket.io

---
+++ pages/admin/events/_id/welcome.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 offset-sm3>
+      <h2 class="ma-3">Selamat Datang di Mozilla Community Space</h2>
+      <p>{{ participant }}</p>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import socket from '~/plugins/socket-io'
+export default {
+  name: 'WelcomeToMozSpace',
+  beforeMount() {
+    socket.on('new-participant', (newParticipant) => {
+      this.participant = newParticipant;
+    })
+  },
+  data() {
+    return {
+      participant: {},
+    }
+  }
+}
+</script>
+
