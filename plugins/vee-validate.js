d4ce55b33b6c7f84abc812292256cb4a533e97a7
dikyarga <dikyarga.id@gmail.com>
2018-04-08 14:52:13 +0700

    localize VeeValidate globally

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+import bahasa from 'vee-validate/dist/locale/id';
+
+Validator.localize('id', bahasa)
 
 Vue.use(VeeValidate)
