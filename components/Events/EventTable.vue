b7890c874b36d43994fdea1ed7d43c06640b8b7b
dikyarga <dikyarga.id@gmail.com>
2018-06-02 00:10:47 +0700

    create event table component with search and pagination than attach it to dashboard

---
+++ components/Events/EventTable.vue
@@ -0,0 +1,117 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm12>
+      <v-card-title>
+        <h2 class="title">{{ title }}</h2>
+        <v-spacer/>
+        <v-text-field
+          append-icon="search"
+          label="Cari"
+          single-line
+          hide-details
+          v-model="searchKeyword"
+        />
+      </v-card-title>
+      <v-data-table
+        :headers="headers"
+        :pagination.sync="pagination"
+        :total-items="totalItems"
+        :loading="loading"
+        :search="searchKeyword"
+        :items="items"
+        hide-actions
+        class="elevation-1">
+        <template slot="items" slot-scope="props">
+          <td>{{ props.item.title }}</td>
+          <td>{{ props.item.startDateTime }}</td>
+          <td>{{ props.item.timeStart }}</td>
+          <td>
+            <v-btn color="primary" @click="goToEventDetail(props.item)">Lihat</v-btn>
+          </td>
+        </template>
+      </v-data-table>
+      <div class="text-xs-center pt-2">
+        <v-pagination v-model="pagination.page" 
+                      :length="pages" 
+                      @next="fetchItems(pagination.page, searchKeyword)" 
+                      @previous="fetchItems(pagination.page, searchKeyword)" 
+                      @input="fetchItems(pagination.page, searchKeyword)"/>
+      </div>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import { mapState, mapActions } from 'vuex';
+
+export default {
+  props: {
+    title: String,
+  },
+  data: () => {
+    return {
+      searchKeyword: '',
+      totalItems: 0,
+      loading: true,
+      pagination: {
+        rowPerPage: 10,
+        page: 1,
+      },
+      items: [],
+      headers: [
+        { text: 'Judul Acara', value: 'title' },
+        { text: 'Tanggal', value: 'startDateTime' },
+        { text: 'Waktu', value: 'timeStart' },
+        { text: 'Aksi', value: '' },
+      ],
+    };
+  },
+  created() {
+    this.fetchItems(1);
+    this.fetchTotalItems();
+  },
+  methods: {
+    ...mapActions(['notify']),
+    fetchItems(page = 1, keyword = '') {
+      const skip = (this.pagination.page - 1) * this.pagination.rowPerPage;
+      const limit = this.pagination.rowPerPage;
+
+      this.loading = true;
+
+      this.$axios
+        .$get(`/Events?filter[limit]=${limit}&filter[skip]=${skip}&filter[where][title][like]=${keyword}&filter[where][title][options]=i`)
+        .then(response => {
+          this.loading = false;
+          this.items = response;
+        })
+        .catch(err => {
+          this.notify({ type: 'error', message: err.message });
+        });
+    },
+    fetchTotalItems() {
+      this.$axios.$get('/Events/count').then(response => {
+        this.totalItems = response.count;
+      })
+    },
+    goToEventDetail(event) {
+      this.$router.push({
+        name: 'admin-events-id'
+      })
+    }
+  },
+  computed: {
+    ...mapState(['token']),
+    pages() {
+      if (this.pagination.rowPerPage == null || this.pagination.totalItems == null) {
+        return 0;
+      }
+
+      return Math.ceil(this.totalItems / this.pagination.rowPerPage);
+    },
+  },
+  watch: {
+    searchKeyword(keyword) {
+      this.fetchItems(1, keyword);
+    },
+  },
+};
+</script>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,3 +1,28 @@
 <template>
-  <h2>Selamat datang admin</h2>
+<div>
+ <v-layout>
+   <v-flex align-center justify-center>
+      <h2 class="text-xs-center">Selamat datang Kiholder</h2>
+   </v-flex>
+ </v-layout>
+  <v-layout align-center justify-center>
+    <v-flex xs12 
+            sm6 
+            md6>
+      <event-table title="Acara Mendatang"/>
+    </v-flex>
+    <v-flex xs12 sm6 md6>
+      <h3>event belum di konfirmasi</h3>
+    </v-flex>
+  </v-layout>
+</div>
 </template>
+<script>
+import EventTable from '@/components/Events/EventTable'
+
+export default {
+  components: {
+    EventTable,
+  } 
+}
+</script>
