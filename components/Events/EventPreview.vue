aaa5ff8c8c38df11cf1c841f2c5cffc15a987e3c
maulidan <maulidan.ksl@gmail.com>
2019-04-07 18:14:15 +0700

    Add actions to the buttons

--- components/Events/EventPreview.vue
+++ components/Events/EventPreview.vue
@@ -5,11 +5,13 @@
     <v-card-title primary-title>
       <div>
         <h3 class="headline mb-0">{{ title }}</h3>
-        <div> {{ description }} </div>
+        <div> {{ description }}  </div>
+
       </div>
     </v-card-title>
     <v-card-actions>
-      <v-btn flat color="secondary" @click="$router.push('/events/' + slug)">Daftar</v-btn>
+      <!--<a  v-if="registrationLink != ''" class="btn btn&#45;&#45;flat secondary&#45;&#45;text" :href="registrationLink" target="_blank">Daftar</a>-->
+      <v-btn v-show="registrationLink != '' " flat color="secondary" v-bind:href="registrationLink">Daftar</v-btn>
       <v-btn flat color="orange">Bagikan</v-btn>
     </v-card-actions>
   </v-card>
@@ -39,7 +41,11 @@ export default {
       type: String,
       required: true,
     },
+    registrationLink :{
+      type: String,
+      required: true,
+    }
   }
-  
+
 }
 </script>
