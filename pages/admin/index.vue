af2fbdedf87c54123ede5a2dba9868bc0c64a8d9
Davin Reinaldo Gozali <davinreinaldogozali@gmail.com>
2019-04-07 17:11:23 +0700

    Fix Typo in dashboard page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -2,7 +2,7 @@
 <div>
  <v-layout row wrap justify-center align-center>
    <v-flex xs12>
-      <h2 class="text-xs-center">Selamat datang Kiholder</h2>
+      <h2 class="text-xs-center">Selamat datang Keyholder</h2>
    </v-flex>
  </v-layout>
   <v-layout row wrap align-center justify-center>
@@ -12,7 +12,7 @@
       <event-table title="Acara Mendatang"/>
     </v-flex>
     <v-flex xs12 sm6 md6>
-      <event-table title="Event belum terkonfimrasi" :confirmed="false"></event-table>
+      <event-table title="Event belum terkonfirmasi" :confirmed="false"></event-table>
     </v-flex>
   </v-layout>
 </div>
